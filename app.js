c249010939bfe15b44a1074480ac49e5cb2fb2cc
Andrew Smith <andrew@codingmba.com>
2018-02-26 20:28:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: "Address to fetch weather for",
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, geocodeResults) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(geocodeResults.address);
+    weather.getWeather(geocodeResults.latitude, geocodeResults.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature} degrees.`);
+        console.log(`It feels like ${weatherResults.apparentTemperature} degrees.`);
+      }
+    });
+  }
+});
